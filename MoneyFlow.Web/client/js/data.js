ea3dbe33455c4c5d6db1c2fd58d29f276363531f
shelldn <yevgeny.shirin@gmail.com>
2014-03-07 22:25:27 +0200

    Подчистил client: скрипты и стили.

--- MoneyFlow.Web/client/js/data.js
+++ MoneyFlow.Web/client/js/data.js
@@ -4,5 +4,18 @@ angular.module('mf.data', ['ngResource']);
 angular.module('mf.data')
 
     .factory('uow', function($resource) {
-        
+        var
+            categories = $resource('/api/categories'),
+            consumptions = $resource('api/consumptions', null, {
+                periods: {
+                    method: 'GET',
+                    url: '/api/consumptions/periods',
+                    isArray: true
+                }
+            });
+
+        return {
+            categories: categories,
+            consumptions: consumptions
+        };
     });
--- MoneyFlow.Web/js/app.js
+++
@@ -1,1 +0,0 @@
-﻿angular.module('mfApp', ['ngResource', 'ui.bootstrap']);
