55c900056200b73e13607bf9a4c0eee3ce9ecdee
shelldn <yevgeny.shirin@gmail.com>
2014-03-10 17:32:59 +0200

    Добавил account/signin.

---
+++ MoneyFlow.Web/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MoneyFlow.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult SignIn(string returnUrl)
+        {
+            return View();
+        }
+    }
+}
