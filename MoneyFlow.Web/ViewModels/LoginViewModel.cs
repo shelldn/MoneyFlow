4b853b4efdf44d046c7af215a2a37db5974a77b4
shelldn <yevgeny.shirin@gmail.com>
2014-03-10 18:36:55 +0200

    Доработал стили формы логина.

--- MoneyFlow.Web/ViewModels/LoginViewModel.cs
+++ MoneyFlow.Web/ViewModels/LoginViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace MoneyFlow.Web.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoneyFlow.Web.ViewModels
 {
     public class LoginViewModel
     {
+        [Display(Name = "Email")]
         public string UserName { get; set; }
+
         public string Password { get; set; }
     }
 }
