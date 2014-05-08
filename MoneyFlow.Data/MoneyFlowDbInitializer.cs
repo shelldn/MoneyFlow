8e069cc26cdbec3915f6b36593e28c841854baf5
shelldn <yevgeny.shirin@gmail.com>
2014-05-08 18:38:32 +0300

    Переписал логику сида категорий с использованием функции MoneyFlow.Seed.Data.Seed().

--- MoneyFlow.Data/MoneyFlowDbInitializer.cs
+++ MoneyFlow.Data/MoneyFlowDbInitializer.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using MoneyFlow.Model;
+﻿using System.Data.Entity;
 
 using SampleData = MoneyFlow.Seed.Data;
 
@@ -10,30 +7,11 @@ namespace MoneyFlow.Data
     public class MoneyFlowDbInitializer 
         : DropCreateDatabaseAlways<MoneyFlowDbContext>
     {
-        protected override void Seed(MoneyFlowDbContext context)
+        protected override void Seed(MoneyFlowDbContext db)
         {
-            var categories = new List<Category>
-            {
-                new Category() { Description = "Продукты" },
-                new Category() { Description = "Корм для собаки" },
-                new Category() { Description = "Кино" }
-            };
+            SampleData.Seed(25, d => db.Categories.AddRange(d.Categories));
 
-            var consumptions = new List<Consumption>
-            {
-                new Consumption() { Date = DateTime.Parse("2013-01-01T13:44:51"), Category = categories[0], Amount = 10.25m },
-                new Consumption() { Date = DateTime.Parse("2013-01-25T20:51:21"), Category = categories[1], Amount = 125.50m },
-                new Consumption() { Date = DateTime.Parse("2013-02-12T23:23:13"), Category = categories[0], Amount = 150m },
-                new Consumption() { Date = DateTime.Parse("2013-02-15T01:11:15"), Category = categories[1], Amount = 235m },
-                new Consumption() { Date = DateTime.Parse("2013-02-10T14:20:15"), Category = categories[2], Amount = 55.35m },
-                new Consumption() { Date = DateTime.Parse("2014-01-02T17:25:21"), Category = categories[2], Amount = 100.25m },
-                new Consumption() { Date = DateTime.Parse("2014-01-04T10:15:10"), Category = categories[1], Amount = 75.40m },
-                new Consumption() { Date = DateTime.Parse("2014-01-06T09:55:01"), Category = categories[0], Amount = 88.50m }
-            
-            };
-
-            context.Consumptions.AddRange(consumptions);
-            context.SaveChanges();
+            db.SaveChanges();
         }
     }
 }
