f40435ffb92084a0df1f0f8118958e3fc1c2c8e0
shelldn <yevgeny.shirin@gmail.com>
2014-05-08 14:59:36 +0300

    Модули Data и XmlReader в проекте F#.

---
+++ MoneyFlow.Seed/Data.fs
@@ -0,0 +1,23 @@
+﻿namespace MoneyFlow.Seed
+
+open System
+open System.Collections.Generic
+open MoneyFlow.Model
+
+module Data =
+    
+    type T = { 
+        Categories : Category seq;
+        Consumptions : Consumption seq
+    }
+
+    type ApplyFunc = delegate of T -> unit
+    
+    let Seed cnsCount (applyFunc : ApplyFunc) =
+
+        let data = { 
+            Categories = read "Categories.xml";
+            Consumptions = [ for i in 0..cnsCount -> Consumption() ]
+        }
+
+        applyFunc.Invoke(data)
