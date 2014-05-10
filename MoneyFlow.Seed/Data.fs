20624a2be81add1ab16dc0393f1ce717ad32edc2
shelldn <yevgeny.shirin@gmail.com>
2014-05-10 14:02:51 +0300

    Доработал логику сидирования Consumption'ов.

--- MoneyFlow.Seed/Data.fs
+++ MoneyFlow.Seed/Data.fs
@@ -4,6 +4,11 @@ open System
 open System.Collections.Generic
 open MoneyFlow.Model
 
+module Consumptions =
+
+    let seed n =
+        [ for i in 0..n -> Consumption() ]
+
 module Data =
     
     type T = { 
@@ -17,7 +22,17 @@ module Data =
 
         let data = { 
             Categories = read<Category> "Categories.xml";
-            Consumptions = [ for i in 0..cnsCount -> Consumption() ]
+            Consumptions = Consumptions.seed cnsCount
         }
+
+        let rnd = Random()
+        let catCount = data.Categories |> Seq.length
+
+        let getDate (min : DateTime) = min.AddHours(float (rnd.Next(8760)) * rnd.NextDouble())
+
+        for cns in data.Consumptions do
+            cns.Category <- data.Categories |> Seq.nth (rnd.Next(catCount))
+            cns.Date <- getDate (DateTime.Parse("1/1/2014"))
+            cns.Amount <- decimal (rnd.Next(500)) + Math.Round(decimal (rnd.NextDouble()), 2)
 
         applyFunc.Invoke(data)
