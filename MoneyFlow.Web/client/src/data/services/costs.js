66ed172b62a15ca702ee40376aa4c9f20de93ca6
shelldn <yevgeny.shirin@gmail.com>
2014-08-11 15:42:26 +0300

    uow.costs: + обработчик ответа с парсером даты.

--- MoneyFlow.Web/client/src/data/services/costs.js
+++ MoneyFlow.Web/client/src/data/services/costs.js
@@ -4,5 +4,21 @@ angular.module('mf.data')
     // Costs
 
     .factory('costs', function($resource) {
-        return $resource('/api/costs', { id: '@id' });
+        return $resource('/api/costs', { id: '@id' }, {
+            query: {
+                method: 'GET',
+                isArray: true,
+                transformResponse: function(data) {
+
+                    return (function(costs) {
+                        costs.forEach(function(c) {
+                            c.date = new Date(c.date);
+                        });
+
+                        return costs;
+
+                    })(angular.fromJson(data));
+                }
+            }
+        });
     });
