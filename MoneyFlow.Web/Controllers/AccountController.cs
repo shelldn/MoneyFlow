069ebdbb39e11bac587c924a84699eeab5625aa3
shelldn <yevgeny.shirin@gmail.com>
2014-10-23 17:13:41 +0300

    Added factory method to Account type.

--- MoneyFlow.Web/Controllers/AccountController.cs
+++ MoneyFlow.Web/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
@@ -14,6 +15,8 @@ namespace MoneyFlow.Web.Controllers
     [RoutePrefix("account")]
     public class AccountController : Controller
     {
+        protected string CallbackUri { get; set; }
+
         public IAuthenticationManager AuthenticationManager
         {
             get { return HttpContext.GetOwinContext().Authentication; }
@@ -21,7 +24,17 @@ namespace MoneyFlow.Web.Controllers
 
         public UserManager<Account, Int32> UserManager
         {
-            get { return new UserManager<Account, int>(new MoneyFlowUserStore()); }
+            get
+            {
+                var um = new UserManager<Account, int>(new MoneyFlowUserStore());
+
+                um.UserValidator = new UserValidator<Account, Int32>(um)
+                {
+                    AllowOnlyAlphanumericUserNames = false
+                };
+
+                return um;
+            }
         }
 
         public MoneyFlowSignInManager SignInManager
@@ -31,8 +44,17 @@ namespace MoneyFlow.Web.Controllers
                 return new MoneyFlowSignInManager(
                     new UserManager<Account, int>(new MoneyFlowUserStore()), 
                     AuthenticationManager
-                ); 
+                );
             }
+        }
+
+        protected override void Initialize(RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+
+            // set the return Uri for the
+            // facebook authentication middleware
+            CallbackUri = Url.Action("SignInExternalCallback");
         }
 
         public ActionResult SignIn(string returnUrl)
@@ -64,16 +86,19 @@ namespace MoneyFlow.Web.Controllers
 
             public ChallengeResult(string redirectUri)
             {
-                AuthConfig = new AuthenticationProperties
-                {
-                    RedirectUri = redirectUri
-                };
+                AuthConfig = new AuthenticationProperties { RedirectUri = redirectUri };
+            }
+
+            private static IAuthenticationManager GetAuthManager(ControllerContext context)
+            {
+                return context.HttpContext
+                    .GetOwinContext()
+                    .Authentication;
             }
 
             public override void ExecuteResult(ControllerContext context)
             {
-                context.HttpContext.GetOwinContext().Authentication
-                    .Challenge(AuthConfig, "Facebook");
+                GetAuthManager(context).Challenge(AuthConfig, "Facebook");
             }
         }
 
@@ -85,7 +110,7 @@ namespace MoneyFlow.Web.Controllers
         [Route("sign-in/facebook")]
         public ActionResult SignInExternal()
         {
-            return Challenge(Url.Action("SignInExternalCallback"));
+            return Challenge(redirectUri: CallbackUri);
         }
 
         [Route("sign-in/facebook/callback")]
@@ -96,8 +121,8 @@ namespace MoneyFlow.Web.Controllers
              * 
              */
 
-            var loginInfo = await AuthenticationManager.GetExternalLoginInfoAsync();
-            var siResult = await SignInManager.ExternalSignInAsync(loginInfo, isPersistent: false);
+            var li = await AuthenticationManager.GetExternalLoginInfoAsync();
+            var siResult = await SignInManager.ExternalSignInAsync(li, isPersistent: false);
 
             switch (siResult)
             {
@@ -105,14 +130,12 @@ namespace MoneyFlow.Web.Controllers
                     return RedirectToAction("Index", "Home");
                 case SignInStatus.Failure:
                 default:
-                    var userName = loginInfo.DefaultUserName;
-                    var account = new Account { UserName = userName };
-
+                    var account = Account.FromLoginInfo(info: li);
                     var result = await UserManager.CreateAsync(account);
 
                     if (result.Succeeded)
                     {
-                        result = await UserManager.AddLoginAsync(account.Id, loginInfo.Login);
+                        result = await UserManager.AddLoginAsync(account.Id, li.Login);
 
                         if (result.Succeeded)
                             await SignInManager.SignInAsync(account, false, false);
