d11239aeefcaaa6ec31f47a3c8b7908b8d90ca0b
shelldn <yevgeny.shirin@gmail.com>
2014-10-27 15:34:36 +0200

    Created mf-expand spec.

---
+++ MoneyFlow.Web/client/test/unit/controls/directiveSpec.js
@@ -0,0 +1,32 @@
+describe('directive: mf-expand', function() {
+    var x, $scope;
+
+    beforeEach(module('mf.controls'));
+
+    beforeEach(inject(function($rootScope, $compile) {
+        $scope = $rootScope.$new();
+
+        x = '<div mf-expand="Expand me">Contents</div>';
+        x = $compile(x)($scope);
+
+        $scope.$digest();
+    }));
+
+    it('Should provide a link with the text of mf-expand attribute', function() {
+        expect(x.find('a:first-child').html()).toBe('Expand me');
+    });
+    
+    it('Should transclude the element\'s contents', function() {
+        expect(x.children('div[ng-transclude]').html()).toBe('Contents');
+    }); 
+
+    it('Should toggle isExpanded when tglExpand() is called', function() {
+        $scope.isExpanded = false;
+
+        $scope.tglExpand();
+        expect($scope.isExpanded).toBeTruthy();
+
+        $scope.tglExpand();
+        expect($scope.isExpanded).toBeFalsy();
+    });
+});
