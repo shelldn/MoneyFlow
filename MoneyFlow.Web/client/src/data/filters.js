1d278ef9eab6ac314f5b09f8701db62c54f2e46b
shelldn <yevgeny.shirin@gmail.com>
2014-12-28 23:55:01 +0200

    Implemented link-filter 'reverse' flag behavior.

--- MoneyFlow.Web/client/src/data/filters.js
+++ MoneyFlow.Web/client/src/data/filters.js
@@ -3,10 +3,12 @@ angular.module('mf.data')
     // Link
 
     .filter('link', function() {
-        return function(input) {
+        return function(input, reverse) {
+            var k = reverse ? -1 : 1;
+
             for (var i = 0; i < input.length; i++) {
-                input[i].prev = input[i - 1];
-                input[i].next = input[i + 1];
+                input[i].prev = input[i - k];
+                input[i].next = input[i + k];
             }
 
             return input;
--- MoneyFlow.Web/client/test/data/filterSpec.js
+++ MoneyFlow.Web/client/test/data/filterSpec.js
@@ -24,4 +24,19 @@ describe('filter: link', function() {
         expect(costs[2]).toEqual(jasmine.objectContaining({ prev: costs[1],     next: undefined }));
     });
 
+    it("should treat sequence as first-is-a-last if 'reverse' flag is set", function() {
+        var items = [
+            // next: undefined
+            { item: 1 },
+            { item: 0 }
+            // prev: undefined
+        ];
+
+        // act
+        items = link(items, true);
+
+        // assert
+        expect(items[0]).toEqual(jasmine.objectContaining({ next: undefined,    prev: items[1]  }));
+        expect(items[1]).toEqual(jasmine.objectContaining({ next: items[0],     prev: undefined }));
+    });
 });
