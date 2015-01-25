82681bf9d480eaf33c58ae15011afe46e7675b12
shelldn <yevgeny.shirin@gmail.com>
2015-01-25 21:29:03 +0200

    Refactored. Implemented AuthCtrl.isAuthorized spec.

--- MoneyFlow.Web/client/test/auth/controllerSpec.js
+++ MoneyFlow.Web/client/test/auth/controllerSpec.js
@@ -1,26 +1,60 @@
 describe('controller: AuthCtrl', function() {
     var $scope, $window,
-        authManager, signInDeferred;
+        Account, authManager;
 
     beforeEach(module('mf.auth'));
 
-    beforeEach(inject(function($controller, $rootScope, _$window_, $q, _authManager_) {
-        // suite globals
-        $scope = $rootScope.$new();
-        $window = _$window_;
-        authManager = _authManager_;
+    beforeEach(inject(function($controller, $rootScope, _$window_, _Account_, _authManager_) {
 
-        spyOn(authManager, 'signIn')
-            .and.callFake(function() {
-                return (signInDeferred = $q.defer()).promise;
-            });
+        $controller('AuthCtrl', {
+            $scope          : $scope          = $rootScope.$new(),
+            $window         : $window         = _$window_,
+            Account         : Account         = _Account_,
+            authManager     : authManager     = _authManager_
+        });
 
-        $controller('AuthCtrl', { $scope: $scope, $window: $window, authManager: authManager });
+        // Go!
         $scope.$digest();
     }));
 
+    describe('isAuthorized', function() {
+
+        it('should be fetched only once on construction', function() {
+
+            spyOn(Account, 'isAuthorized')
+                .and.returnValue(false);
+
+            // act
+            var results = [
+                $scope.isAuthorized,    // 1st getter invoke
+                $scope.isAuthorized     // 2nd getter invoke
+            ];
+
+            // assert
+            expect(first(results)).toBe(second(results));
+        });
+
+        it('should return the current account authorization status', function() {
+            var isAuthorized = true;
+
+            spyOn(Account, 'isAuthorized')
+                .and.returnValue(isAuthorized);
+
+            // act/assert
+            expect($scope.isAuthorized).toBe(isAuthorized);
+        });
+    });
+
     describe('signIn()', function() {
-        
+        var signInDeferred;
+
+        beforeEach(inject(function($q) {
+            spyOn(authManager, 'signIn')
+                .and.callFake(function() {
+                    return (signInDeferred = $q.defer()).promise;
+                });
+        }));
+
         it('should reload page on successful sign_in', function() {
 
             spyOn($window.location, 'reload');
