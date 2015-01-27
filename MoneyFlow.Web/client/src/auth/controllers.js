048af85659e0f33ff6e7ef5c0821baa4e752bfdd
shelldn <yevgeny.shirin@gmail.com>
2015-01-28 00:10:10 +0200

    Implemented AuthCtrl.isAuthorized property.

--- MoneyFlow.Web/client/src/auth/controllers.js
+++ MoneyFlow.Web/client/src/auth/controllers.js
@@ -2,7 +2,19 @@ angular.module('mf.auth')
 
     // Authentication
 
-    .controller('AuthCtrl', function($scope, $window, authManager) {
+    .controller('AuthCtrl', function($scope, $window, Account, authManager) {
+
+        var _isAuthorized;
+
+        Object.defineProperty($scope, 'isAuthorized', {
+            enumerable: true,
+            get: function() {
+                return _.isUndefined(_isAuthorized) ?
+                    _isAuthorized = Account.isAuthorized() :
+                    _isAuthorized;
+            }
+        });
+
         $scope.signIn = function() {
             authManager.signIn({
                 userName: $scope.userName,
